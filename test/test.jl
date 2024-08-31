57346cf1368a49e99d22dc5c709d23b6c88ac1f9
shashankku414 <shashank11297@gmail.com>
2024-08-31 18:07:28 -0400

    testing

---
+++ test/test.jl
@@ -0,0 +1,14 @@
+using Test
+using QuadGK
+
+include("../src/Simpson.jl")
+
+
+x = collect(range(0, 9, length=101))
+y = map(t -> (cos(t))^2, x)
+int_exact = 4.3123
+
+quad_int = quadgk(t -> (cos(t))^2, 0, 9, rtol=1e-6)[1]
+
+@test round(Simpson.simpson(y, x), digits=4) ≈ int_exact
+@test isapprox(Simpson.simpson(y, x), quad_int, rtol=1e-4)
