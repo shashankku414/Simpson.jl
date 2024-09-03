293af40e0b72392abeb180549f7c429953e9cdf6
shashankku414 <shashank11297@gmail.com>
2024-09-02 22:42:34 -0400

    test set

--- test/test.jl
+++ test/test.jl
@@ -2,14 +2,14 @@ using Test
 using QuadGK
 using Trapz
 
-include("../src/Simpson.jl")
+include("../src/Simpson38.jl")
 
 
-x = collect(range(0, 9, length=101))
+x = collect(range(0, 9, length=102))
 y = map(t -> (cos(t))^2, x)
-int_exact = 4.3123
+int_exact = 4.31225
 
 quad_int = quadgk(t -> (cos(t))^2, 0, 9, rtol=1e-6)[1]
 
-@test round(Simpson.simpson(y, x), digits=4) ≈ int_exact
-@test isapprox(Simpson.simpson(y, x), quad_int, rtol=1e-4)
+@test round(Simpson38.simpson38(y, x), digits=5) ≈ int_exact
+@test isapprox(Simpson38.simpson38(y, x), quad_int, rtol=1e-4)
